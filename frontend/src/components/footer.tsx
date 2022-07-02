b971a5aad3cf5bef510c2615b9eef4574d3dc63d
Rustam <rustam.mamedov230@gmail.com>
2022-07-02 10:27:57 -0400

    Update github link

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -4,7 +4,7 @@ import { Icon } from 'semantic-ui-react';
 const Footer = () => {
   return (
     <div className='footer'>
-        <a href="https://github.com/Nollum" target="_blank">
+        <a href="https://github.com/Nollum/spry" target="_blank">
           <Icon name='github' color='black' size='big' />
         </a>
     </div>
